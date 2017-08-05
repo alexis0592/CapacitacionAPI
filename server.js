adf68dd702e9cffa1b0e515e709e14c83288c863
Yefry Alexis Calderon Yepes <alexiscalderon@MacBook-Pro-de-Yefry.local>
2017-08-05 14:20:14 -0500

    adding cors origin allows

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose.connect(dbPath, {
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    next();
+});
+
 routes(app);
 
 app.use(function(req, res) {
